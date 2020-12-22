9cd53dd7b39e1da7424e84bcf6f69c8215fc8639
Gregory Brunner <gbrunner@esri.com>
2020-12-22 10:54:47 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+var map = L.map('map').setView([42.35, -71.08], 13);
+
+var Stamen_Terrain = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 18,
+	ext: 'png'
+}).addTo(map);
+
+$.getJSON('https://raw.githubusercontent.com/gbrunner/adv-python-for-gis-and-rs/master/Week%201/rodents.geojson', function(data) {
+  var coordinatesOnly = data.features.map(function(feature) {
+    return [feature.geometry.coordinates[1], feature.geometry.coordinates[0], 1];
+  });
+
+  var heat = L.heatLayer(coordinatesOnly).addTo(map);
+});
+
+
+// make it so the heatmap becomes points at a certain zoom scale
+// hint: https://gis.stackexchange.com/questions/258515/show-hide-markers-depending-on-zoom-level
